8ac11d5d0c4b70a95095ecd3e7d590a4e88abb19
Christiana <kristiana.095@gmail.com>
2019-11-06 17:35:12 +0000

    Included printing data from csv file and csv file

--- src/main/java/io/turntabl/tcms/ClientManagementSystem.java
+++ src/main/java/io/turntabl/tcms/ClientManagementSystem.java
@@ -1,5 +1,8 @@
 package io.turntabl.tcms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientManagementSystem {
@@ -20,7 +23,7 @@ public class ClientManagementSystem {
         //loop the utility in loop until the user makes the choice to exit
         while(true){
             //Print the options for the user to choose from
-            System.out.println("*****Welcome, Sam Moorehouse.*****");
+            System.out.println("*****Welcome, Sam Moorhouse.*****");
 
             System.out.println("*****Available Options*****");
             System.out.println("*. Press 1 to Enter User Details");
@@ -31,28 +34,46 @@ public class ClientManagementSystem {
             System.out.println("Enter your choice:");
 
             //Capture the user input in scanner object and store it in a pre declared variable
-            userInput = sn.next();
+            userInput = sn.nextLine();
 
             //Check the user input
             switch(userInput){
                 case "1":
                     //Enter Client Details
                     System.out.println("*.Enter Client Name. *");
-                    clientName = sn.next();
+                    clientName = sn.nextLine();
                     System.out.println("*.Enter Client Address. *");
-                    clientAddress = sn.next();
+                    clientAddress = sn.nextLine();
                     System.out.println("*.Enter Telephone Number. *");
-                    clientTelephoneNumber = sn.next();
+                    clientTelephoneNumber = sn.nextLine();
                     System.out.println("*.Enter Email Address. *");
-                    clientEmailAddress = sn.next();
+                    clientEmailAddress = sn.nextLine();
                     System.out.println("Details saved successfully.");
                     break;
                 case "2":
+//                   Client searchClient = searchClient.searchClient(1,"Jude");
+
                     //Search for a Client
-                    System.out.println("Client found or not ...");
+//                    System.out.println("Client found or not ...");
                     break;
                 case "3":
+                    //List of Clients
+                    String fileName = "clients.csv";
+                    File file = new File(fileName);
+                    try{
+                        Scanner inputStream = new Scanner(file);
+                        while(inputStream.hasNext()){
+                            String data = inputStream.next();
+                            String[] values =data.split(",");
+                            System.out.println(data);
+                        }
+                        inputStream.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
                     //Delete a Client
+                    //Code goes here
                     System.out.println("Client has been Deleted Successfully");
                     break;
                 case "4":
