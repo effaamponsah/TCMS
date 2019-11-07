5e94813e11c7681b25d89eb87abdebb1dc093d24
FrancisBilla <francis.billa.fb@gmail.com>
2019-11-07 08:16:46 +0000

    gradle successful

--- src/test/java/io/turntabl/tcms/TcmTest.java
+++ src/test/java/io/turntabl/tcms/TcmTest.java
@@ -4,6 +4,8 @@ package io.turntabl.tcms;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
@@ -14,7 +16,7 @@ public class TcmTest {
     @Test
     public void testClient() throws IOException {
         Client c = new Client("glo","54422","gaga@gmail","fafd3444");
-        assertEquals("Jim", c.nameToWriteToFile());
+        assertEquals("glo", c.getName());
     }
 
     @Test
