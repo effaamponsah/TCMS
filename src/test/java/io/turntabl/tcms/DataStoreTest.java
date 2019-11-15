b1ef226b47e1c8364182d5bce06ba8b751414301
bill <teebill15@gmail.com>
2019-11-15 15:24:26 +0100

    adding CRUD methods to TCMS

--- src/test/java/io/turntabl/tcms/DataStoreTest.java
+++ src/test/java/io/turntabl/tcms/DataStoreTest.java
@@ -1,6 +1,5 @@
 package io.turntabl.tcms;
 
-import org.h2.util.ScriptReader;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -8,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
+
 //import com.ibatis.common.jdbc.ScriptRunner;
 import static org.junit.Assert.*;
 
@@ -49,7 +49,7 @@ public class DataStoreTest {
             Class.forName("org.h2.Driver");
             try(Connection db = DriverManager.getConnection(dbUrl,"","")) {
                     Statement s = db.createStatement();
-                    ResultSet rs = s.executeQuery("select * from client limit 3;");
+                    ResultSet rs = s.executeQuery("select * from client;");
 //                    System.out.println("---------------------------------------------------------------------------------------------");
 //                    System.out.printf("%5s %15s %15s %10s %10s", "CUST_ID", "CONTACT_NAME",  "CITY", "COUNTRY", "PHONE");
 //                    System.out.println();
@@ -60,7 +60,76 @@ public class DataStoreTest {
                     }
                 } catch (SQLException e) {
                     System.err.println("Connection error "+e);
-                } ;
+                }
+
+
+        }
+
+        @Test
+        public void testDeleteQuery() throws ClassNotFoundException{
+            Class.forName("org.h2.Driver");
+            try(Connection db = DriverManager.getConnection(dbUrl,"","")) {
+                Statement s = db.createStatement();
+                boolean rs = s.execute("delete from client where ID = 2");
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                    System.out.printf("%5s %15s %15s %10s %10s", "CUST_ID", "CONTACT_NAME",  "CITY", "COUNTRY", "PHONE");
+//                    System.out.println();
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                while(rs.next()){
+//                    System.out.format("%5s %17s %15s %10s %15s", rs.getString("ID"), rs.getString("fName"), rs.getString("lName"), rs.getString("address"), rs.getString("email"));
+//                    System.out.println();
+//                }
+                System.out.println(rs);
+
+            } catch (SQLException e) {
+                System.err.println("Connection error "+e);
             }
+
+
+        }
+
+        @Test
+    public void testUpdateQuery() throws ClassNotFoundException{
+            Class.forName("org.h2.Driver");
+            try(Connection db = DriverManager.getConnection(dbUrl,"","")) {
+                Statement s = db.createStatement();
+                boolean rs = s.execute("update client set fName = 'Francis' where ID =1");
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                    System.out.printf("%5s %15s %15s %10s %10s", "CUST_ID", "CONTACT_NAME",  "CITY", "COUNTRY", "PHONE");
+//                    System.out.println();
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                while(rs.next()){
+//                    System.out.format("%5s %17s %15s %10s %15s", rs.getString("ID"), rs.getString("fName"), rs.getString("lName"), rs.getString("address"), rs.getString("email"));
+//                    System.out.println();
+//                }
+                System.out.println(rs);
+
+            } catch (SQLException e) {
+                System.err.println("Connection error "+e);
+            }
+
+        }
+
+        @Test
+    public void testInsertQuery() throws ClassNotFoundException{
+            Class.forName("org.h2.Driver");
+            try(Connection db = DriverManager.getConnection(dbUrl,"","")) {
+                Statement s = db.createStatement();
+                boolean rs = s.execute("insert into client(fName,lName,address,telephone,email) values('yes', 'no', 'Burkina','0275876542', 'francis@gmail.com')");
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                    System.out.printf("%5s %15s %15s %10s %10s", "CUST_ID", "CONTACT_NAME",  "CITY", "COUNTRY", "PHONE");
+//                    System.out.println();
+//                    System.out.println("---------------------------------------------------------------------------------------------");
+//                while(rs.next()){
+//                    System.out.format("%5s %17s %15s %10s %15s", rs.getString("ID"), rs.getString("fName"), rs.getString("lName"), rs.getString("address"), rs.getString("email"));
+//                    System.out.println();
+//                }
+                System.out.println(rs);
+
+            } catch (SQLException e) {
+                System.err.println("Connection error "+e);
+            }
+
+        }
 
 }
